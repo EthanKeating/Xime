19d47b92208ce85cc75379f5918083cb5c75d35c
EthanKeating <ethankeating1313@gmail.com>
2024-12-27 10:49:41 -0500

    whitelist

--- src/main/java/club/mcgamer/xime/command/CommandHandler.java
+++ src/main/java/club/mcgamer/xime/command/CommandHandler.java
@@ -14,6 +14,7 @@ public class CommandHandler {
     public CommandHandler(XimePlugin plugin) {
         this.plugin = plugin;
 
+        new WhitelistCommand();
         new MapCommand();
         new JoinCommand();
         new HubCommand();
